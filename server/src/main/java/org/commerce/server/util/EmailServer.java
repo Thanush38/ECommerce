594e6133769cb2d2729d2ae16cc1a9a3b53072e9
thanush <thanush38@outlook.com>
2024-05-21 13:32:00 -0400

    added Mail file

--- server/src/main/java/org/commerce/server/util/EmailServer.java
+++ server/src/main/java/org/commerce/server/util/EmailServer.java
@@ -8,10 +8,17 @@ import jakarta.mail.Transport;
 import jakarta.mail.internet.InternetAddress;
 import jakarta.mail.internet.MimeMessage;
 import jakarta.mail.Authenticator;
+import org.json.JSONObject;
+import org.json.JSONArray;
 public class EmailServer {
+    ProductReader reader = new ProductReader();
     public String sendEmail(String data) {
         System.out.println("Email sent: " + data);
 
         return "Email sent";
+    }
+
+    public JSONObject getTokens(){
+        reader.readFileContents("hidden/Mail.json");
     }
 }
