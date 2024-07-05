4209b85942633ab3b26447fdee3c3a876c0232c5
thanush <thanush38@outlook.com>
2024-07-05 13:11:05 -0400

    added new renamed server

---
+++ serverrefactor/Models/EmailData.cs
@@ -0,0 +1,9 @@
+namespace WebApplication1.Models;
+
+public class EmailData
+{
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Message { get; set; }
+    
+}
---
+++ serverrefactor/utils/EmailRequest 2.cs
@@ -0,0 +1,8 @@
+namespace Utils;
+
+public class EmailRequest
+{
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Message { get; set; }
+}
